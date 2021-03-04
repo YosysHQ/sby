1ffef12cf1305da36c96b3f3cb20fdfacac820d2
Claire Xen <claire@clairexen.net>
2021-03-04 16:49:31 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'SymbiYosys'
-copyright = '2021 YosysHQ'
-author = 'YosysHQ'
+#copyright = '2021 YosysHQ'
+#author = 'YosysHQ'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
