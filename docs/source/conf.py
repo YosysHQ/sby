66a458958dc93f8e12418d425e4c31848889937b
Claire Xenia Wolf <claire@clairexen.net>
2021-05-21 03:36:11 +0200

    Update docs conf.py
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-project = 'YosysHQ-AppNote-120'
-author = 'YosysHQ Gmbh'
-copyright ='2021 YosysHQ Gmbh'
+project = 'YosysHQ SBY'
+author = 'YosysHQ GmbH'
+copyright ='2021 YosysHQ GmbH'
 
 # select HTML theme
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
@@ -15,7 +15,7 @@ highlight_language = 'systemverilog'
 
 html_theme_options = {
     'extra_nav_links' : { 
-        'Documentation Index' : 'https://yosyshq-docs.readthedocs.io' ,
+        'YosysHQ Docs' : 'https://yosyshq-docs.readthedocs.io' ,
         'YosysHQ Website' : 'https://www.yosyshq.com',
     },
 #   'logo' : 'logo.png',
