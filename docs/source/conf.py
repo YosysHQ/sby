882d5b3a965a18a8d6efb24325d65d137fb35928
Claire Xenia Wolf <claire@clairexen.net>
2021-11-26 20:34:55 +0100

    update docs theme
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,20 +4,23 @@ author = 'YosysHQ GmbH'
 copyright ='2021 YosysHQ GmbH'
 
 # select HTML theme
-#html_theme = 'alabaster'
+html_theme = 'press'
+html_logo = '../static/logo.png'
+html_favicon = '../static/favico.png'
+html_css_files = ['yosyshq.css']
+html_sidebars = {'**': ['util/searchbox.html', 'util/sidetoc.html']}
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+html_static_path = ['../static', "../images"]
 
 # code blocks style 
 pygments_style = 'colorful'
 highlight_language = 'systemverilog'
 
 html_theme_options = {
-    'extra_nav_links' : { 
-        'YosysHQ Docs' : 'https://yosyshq.readthedocs.io' ,
-        'YosysHQ Website' : 'https://www.yosyshq.com',
-    },
-#   'logo' : 'logo.png',
-    'fixed_sidebar' : True,
+    'external_links' : [
+        ('YosysHQ Docs', 'https://yosyshq.readthedocs.io'),
+        ('Blog', 'https://blog.yosyshq.com'),
+        ('Website', 'https://www.yosyshq.com'),
+    ],
 }
