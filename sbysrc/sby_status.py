17af0f18f2975b06c1640c734fddbf107b308a6b
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-06-18 11:57:10 +1200

    Add note to bad schema exception
    Requires python >= 3.11 (oss-cad-suite is 3.11.6, but there isn't any minimum python version given for SBY that I can find).
    Makes the error less opaque (even though it still has the trace), which I think is necessary given that using the status db is totally optional and otherwise using a different version of SBY with an extra db field in a directory where a previous version has run will just crash with an obscure sqlite3.OperationalError.

--- sbysrc/sby_status.py
+++ sbysrc/sby_status.py
@@ -69,6 +69,9 @@ def transaction(method: Fn) -> Fn:
             self.log_debug(f"failed {method.__name__!r} transaction {err}")
             if not isinstance(err, sqlite3.OperationalError):
                 raise
+            if re.match("table \w+ has no column named \w+", err.args[0]):
+                err.add_note("SBY status database can be reset with --statusreset")
+                raise
         else:
             self.log_debug(f"comitted {method.__name__!r} transaction")
             return result
