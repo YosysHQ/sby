911ae02ee5d068ad82b0f46f54b275a20599ab6a
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-07-05 12:40:57 +1200

    Test property statuses for cover_assert
    Cover properties shouldn't be marked fail when the test failed early due to an assertion.
    This should fail without other changes.

---
+++ tests/statusdb/mixed.v
@@ -0,0 +1,16 @@
+module test (input CP, CN, input A, B, output reg XP, XN);
+	reg [7:0] counter = 0;
+	always @* begin
+		assume (A || B);
+		assume (!A || !B);
+		assert (A == B);
+		cover (counter == 3 && A);
+		cover (counter == 3 && B);
+	end
+	always @(posedge CP)
+		counter <= counter + 1;
+	always @(posedge CP)
+		XP <= A;
+	always @(negedge CN)
+		XN <= B;
+endmodule
