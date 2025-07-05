b80a843995436727e4ddf9bee992c493215b860a
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-07-05 15:46:40 +1200

    tests/links: heredocs are never linked

--- tests/links/symlink.py
+++ tests/links/symlink.py
@@ -6,7 +6,13 @@ def main():
     workdir, task = sys.argv[1:]
     src = Path(workdir) / "src"
     for srcfile in src.iterdir():
-        assert(srcfile.is_symlink() == (task == "link"))
+        if srcfile.name == "heredoc":
+            assert(not srcfile.is_symlink())
+            with open(srcfile, "r") as f:
+                local_contents = f.readline()
+                assert(local_contents.strip() == 'log foo')
+        else:
+            assert(srcfile.is_symlink() == (task == "link"))
 
 if __name__ == "__main__":
     main()
