a215e3260a336c3b72e7b0f7f2db3c0ce6855ad3
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-08-02 10:07:06 +1200

    tests/links/symlink: Check file count
    And also that `src/dir/script.ys` exists

--- tests/links/symlink.py
+++ tests/links/symlink.py
@@ -4,6 +4,7 @@ import sys
 def main():
     workdir, task = sys.argv[1:]
     src = Path(workdir) / "src"
+    count = 0
     for srcfile in src.iterdir():
         if srcfile.name == "heredoc":
             assert(not srcfile.is_symlink())
@@ -13,6 +14,10 @@ def main():
         else:
             assert(srcfile.is_symlink() == (task == "link"))
             assert(srcfile.name != "script.ys")
+        count += 1
+    assert(count == 4)
+    script_ys = src / "dir" / "script.ys"
+    assert(script_ys.exists())
 
 if __name__ == "__main__":
     main()
